5e3cae848d608dd7419f42c0d2da706d85051c38
Satyam Shree <s.shree@castsoftware.com>
2025-05-26 18:30:57 +0530

    Fix all apis, fix filter on tags , fix blob data on user

--- models/artist_info.go
+++ models/artist_info.go
@@ -1,12 +1,31 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"gorm.io/gorm"
+)
+
+type JSONMap map[string]string
+
+func (j JSONMap) Value() (driver.Value, error) {
+	return json.Marshal(j)
+}
+
+func (j *JSONMap) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return fmt.Errorf("Failed to unmarshal JSONMap value: %v", value)
+	}
+	return json.Unmarshal(bytes, j)
+}
 
 type ArtistInfo struct {
 	gorm.Model
-	Name            string            `json:"name"`
-	Bio             string            `json:"bio"` // markdown content
-	Email           string            `json:"email"`
-	SocialLinks     map[string]string `gorm:"type:jsonb" json:"social_links"` // e.g., {"instagram": "...", "twitter": "..."}
-	ProfileImageURL string            `json:"profile_image_url"`
+	Name            string  `json:"name"`
+	Bio             string  `json:"bio"` // markdown content
+	Email           string  `json:"email"`
+	SocialLinks     JSONMap `gorm:"type:jsonb" json:"social_links"`
+	ProfileImageURL string  `json:"profile_image_url"`
 }
--- utils/jwt.go
+++ utils/jwt.go
@@ -8,10 +8,9 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var SecretKey = []byte(os.Getenv("JWT_SECRET"))
-
 func GenerateJWT(userID uint) (string, error) {
 	expiryHoursStr := os.Getenv("JWT_EXPIRY_HOURS")
+	SecretKey := []byte(os.Getenv("JWT_SECRET"))
 	expiryHours, err := strconv.Atoi(expiryHoursStr)
 	if err != nil || expiryHours <= 0 {
 		expiryHours = 72 // fallback default
